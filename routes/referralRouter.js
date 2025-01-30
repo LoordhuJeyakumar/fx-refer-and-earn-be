e32d203a270232fb29c978dff2dc6bb61e8dfccc
Loordhu Jeyakumar <loordhujeyakumar@gmail.com>
2025-01-30 16:32:47 +0530

    refactor referral routes to improve structure and add new endpoints for stats and listing

--- routes/referralRouter.js
+++ routes/referralRouter.js
@@ -1,4 +1,8 @@
-const referralController = require("../controllers/referralController");
+const {
+  createReferral,
+  getReferralStats,
+  listReferrals,
+} = require("../controllers/referralController");
 const {
   validateReferralData,
   validateReferral,
@@ -6,12 +10,23 @@ const {
 
 const referralRouter = require("express").Router();
 
-// Route to create a new referral
+/* // Route to create a new referral
 referralRouter.post(
   "/createReferral",
   validateReferralData, // Middleware to validate referral data
   validateReferral, // Middleware to check validation results
   referralController.createReferral // Controller method to handle referral creatio
+); */
+
+referralRouter.post(
+  "/create",
+  validateReferralData,
+  validateReferral,
+  createReferral
 );
+
+referralRouter.get("/stats/:userId", getReferralStats);
+
+referralRouter.get("/list/:userId", listReferrals);
 
 module.exports = referralRouter;
