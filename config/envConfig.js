8e30dadb56d51dd7401407e5deeb66e468a8b51f
Loordhu Jeyakumar <loordhujeyakumar@gmail.com>
2025-01-30 16:33:54 +0530

    add email configuration variables to envConfig

--- config/envConfig.js
+++ config/envConfig.js
@@ -5,7 +5,19 @@ const dotEnv = require("dotenv");
 dotEnv.config();
 
 const PORT = process.env.PORT;
+const EMAIL_HOST = process.env.EMAIL_HOST;
+const EMAIL_PORT = process.env.EMAIL_PORT;
+const EMAIL_SECURE = process.env.EMAIL_SECURE;
+const EMAIL_USER = process.env.EMAIL_USER;
+const EMAIL_PASSWORD = process.env.EMAIL_PASSWORD;
+const FRONTEND_BASEURI = process.env.FRONTEND_BASEURI;
 
 module.exports = {
   PORT,
+  EMAIL_HOST,
+  EMAIL_PORT,
+  EMAIL_SECURE,
+  EMAIL_USER,
+  EMAIL_PASSWORD,
+  FRONTEND_BASEURI,
 };
